dbfdb6cf58bcf166f82ed060a2e2b79a9eece91d
jsy0404 <pnmsegwyd@gmail.com>
2021-06-21 03:31:07 +0000

    fix: Import path

--- soy/server.go
+++ soy/server.go
@@ -1,8 +1,9 @@
-package Server
+package main
 
 import (
     "github.com/soyoslab/soy_log_collector/soy/lib"
 	"github.com/smallnest/rpcx/server"
+	"flag"
 )
 
 var addr = flag.String("addr", "localhost:8972", "server address")

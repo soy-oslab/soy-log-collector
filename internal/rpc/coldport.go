5bf272ebb8b094829fc998b2ca7ad15ff78626d2
jsy0404 <pnmsegwyd@gmail.com>
2021-06-30 06:52:45 +0000

    feat : add coldport for rpc
    Add coldport for rpc.
    Data should be compacted.

---
+++ internal/rpc/coldport.go
@@ -0,0 +1,28 @@
+package rpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/soyoslab/soy_log_collector/internal/global"
+	"github.com/soyoslab/soy_log_collector/internal/util"
+)
+
+// ColdPort is rpc procedure type.
+// Data should be compacted.
+type ColdPort int
+
+// Push insert data from rpc call into ring buffer.
+// Return error when ColdPort is full.
+// Communicate with caller via reply.
+// Send current ColdPort utility with reply
+func (t *ColdPort) Push(ctx context.Context, args *LogMessage, reply *Reply) error {
+	if global.ColdRing.Size() >= global.DefaultRingSize {
+		reply.Rate = util.RangeMapping(global.ColdRing.Size())
+		return errors.New("ColdPort is full!")
+	}
+	log := CopyLogMessage(args)
+	global.ColdRing.Push(log)
+	reply.Rate = util.RangeMapping(global.ColdRing.Size())
+	return nil
+}
